bf31ff1a6c3e2f5460c7be375cd50308dfb943b6
siriseleniun <sirishakotha9@gmail.com>
2020-03-18 11:22:28 -0700

    staging new files to invoke pom.xml

---
+++ mmp/src/main/java/com/iit/mmp/mmpproject/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package com.iit.mmp.mmpproject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	WebDriver driver;
+	
+	public LoginPage(WebDriver driver) {
+		
+		this.driver= driver;
+	}
+	
+	public void login(String userName, String password) {
+		
+			driver.findElement(By.id("username")).sendKeys(userName);	
+			
+			driver.findElement(By.id("password")).sendKeys(password);
+			
+			driver.findElement(By.name("submit")).click();
+			
+		}
+}
